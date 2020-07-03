9ad68ce8f5cb91e8418c84937af257ff469e77b9
Melissa Gu <melissa.gu@richmond.edu>
2020-07-03 11:02:02 -0400

    routing for navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 from app.models import EditableHTML
 
@@ -7,11 +7,22 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('main/index.html')
+    return redirect('/daily')
 
-
-@main.route('/about')
-def about():
-    editable_html_obj = EditableHTML.get_editable_html('about')
+@main.route('/daily')
+def daily():
+    editable_html_obj = EditableHTML.get_editable_html('daily')
     return render_template(
-        'main/about.html', editable_html_obj=editable_html_obj)
+        'main/daily.html', editable_html_obj=editable_html_obj)
+
+@main.route('/monthly')
+def monthly():
+    editable_html_obj = EditableHTML.get_editable_html('monthly')
+    return render_template(
+        'main/monthly.html', editable_html_obj=editable_html_obj)
+
+@main.route('/habits')
+def habits():
+    editable_html_obj = EditableHTML.get_editable_html('habits')
+    return render_template(
+        'main/habits.html', editable_html_obj=editable_html_obj)
